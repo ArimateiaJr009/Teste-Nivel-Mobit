37036ce3926b984e79a3284fe6adf28b74733ba0
arimateia <juniorcs09@gmail.com>
2023-02-06 16:07:42 -0300

    adicionando questao 01 comentada

---
+++ Questao_01/conf_crontab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#imaginando que o script ficar em /user/sbin (default ubuntu) faz-se os seguintes comandos
+
+PATH_Q1=/user/sbin/Q1
+echo $PATH_Q1
+
+#(crontab -l ; echo "* 23 * * * $PATH_Q1") | crontab -
+
